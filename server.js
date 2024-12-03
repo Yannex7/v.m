7f3d523ac54dea5e89d149eb841f0a8412ba3d26
Yannex7 <142844872+Yannex7@users.noreply.github.com>
2024-12-03 22:48:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ app.use(express.json());
 const uri = "mongodb+srv://yannismartinils:D5HMpNx1wVCZyfme@vape-db.y7lnc.mongodb.net/?retryWrites=true&w=majority&appName=vape-db";
 const client = new MongoClient(uri);
 
-// Grundstruktur der Daten
-const initialData = {
+let currentData = {
     stock: { robin: 0, robink: 0, adrian: 0, andreas: 0 },
     sales: { robin: 0, robink: 0, adrian: 0, andreas: 0 },
     consumed: { robin: 0, robink: 0, adrian: 0, andreas: 0 },
@@ -22,8 +21,9 @@ app.post('/api/data', async (req, res) => {
         await client.connect();
         const database = client.db('vape-db');
         const collection = database.collection('vape-data');
-        await collection.updateOne({}, { $set: req.body }, { upsert: true });
-        res.json({ message: 'Daten gespeichert' });
+        currentData = req.body;
+        await collection.updateOne({}, { $set: currentData }, { upsert: true });
+        res.json({ success: true, data: currentData });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
@@ -35,7 +35,10 @@ app.get('/api/data', async (req, res) => {
         const database = client.db('vape-db');
         const collection = database.collection('vape-data');
         const data = await collection.findOne({});
-        res.json(data || initialData);
+        if (data) {
+            currentData = data;
+        }
+        res.json(currentData);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
