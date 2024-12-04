1bfa14cb0034808b4f3df318327d9e0aa4334377
Yannex7 <142844872+Yannex7@users.noreply.github.com>
2024-12-04 16:24:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ app.use(cors());
 app.use(express.json());
 
 const uri = "mongodb+srv://yannismartinils:D5HMpNx1wVCZyfme@vape-db.y7lnc.mongodb.net/?retryWrites=true&w=majority&appName=vape-db";
-const client = new MongoClient(uri);
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 const defaultData = {
     stock: { robin: 0, robink: 0, adrian: 0, andreas: 0 },
@@ -16,11 +19,21 @@ const defaultData = {
     payments: { robin: 0, robink: 0, adrian: 0, andreas: 0 }
 };
 
+let dbConnection;
+
+async function connectDB() {
+    if (!dbConnection) {
+        await client.connect();
+        dbConnection = client.db('vape-db');
+        console.log('Connected to MongoDB');
+    }
+    return dbConnection;
+}
+
 app.get('/api/data', async (req, res) => {
     try {
-        await client.connect();
-        const database = client.db('vape-db');
-        const collection = database.collection('vape-data');
+        const db = await connectDB();
+        const collection = db.collection('vape-data');
         let data = await collection.findOne({});
         if (!data) {
             await collection.insertOne(defaultData);
@@ -28,18 +41,19 @@ app.get('/api/data', async (req, res) => {
         }
         res.json(data);
     } catch (error) {
+        console.error('Database error:', error);
         res.json(defaultData);
     }
 });
 
 app.post('/api/data', async (req, res) => {
     try {
-        await client.connect();
-        const database = client.db('vape-db');
-        const collection = database.collection('vape-data');
+        const db = await connectDB();
+        const collection = db.collection('vape-data');
         await collection.updateOne({}, { $set: req.body }, { upsert: true });
         res.json({ success: true });
     } catch (error) {
+        console.error('Save error:', error);
         res.status(500).json({ error: error.message });
     }
 });
